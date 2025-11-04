7374e182cb9343e7cebe41c6d81fbf2edbc95945
jm-merchan <jose.merchan@hashicorp.com>
2025-11-04 17:29:26 +0100

    allow rpc nomad clients

--- 1_create_clusters/main.tf
+++ 1_create_clusters/main.tf
@@ -165,6 +165,14 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Nomad RPC"
+    from_port   = 4647
+    to_port     = 4647
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
